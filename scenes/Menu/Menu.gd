edbd8526facda36ced37451bc458b313f9c6d522
lucagm94 <luca-moyano@hotmail.com>
2023-05-09 07:24:02 -0300

    Se creo la función "count_new_instance" en el script de la escena "Menu"

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -9,3 +9,7 @@ func _ready():
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	pass
+	
+func count_new_instance() -> void: 
+	instancesCount += 1
+	
