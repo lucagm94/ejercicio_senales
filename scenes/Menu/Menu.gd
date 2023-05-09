b48e5e00002f9bcec418337d3f3a3785b1bb785d
lucagm94 <luca-moyano@hotmail.com>
2023-05-09 07:29:50 -0300

    Se conectaron manualmente las señales "button_down"  de "RedGenerator" y "VioletGenerator" con la función "count_new_instance"

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -8,8 +8,10 @@ func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	$RedGenerator.connect("button_down", count_new_instance)
+	$VioletGenerator.connect("button_down", count_new_instance)
 	pass
 	
 func count_new_instance() -> void: 
 	instancesCount += 1
-	
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
